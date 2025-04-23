7f59457235f762296f6846eefbe3f871a70311e2
Tam1988 <tamaraajaluwa@gmail.com>
2025-04-23 12:16:33 +0100

    Completed OOP Assignment On Python For Week 5

--- Main.py
+++ Main.py
@@ -13,19 +13,22 @@ def main():
     library.add_book(Audiobook("The Lord of the Rings", "J.R.R. Tolkien", 1954, 20))
     library.add_book(Audiobook("One Hundred Years of Solitude", "Gabriel García Márquez", 1967, 17))
     library.add_book(Audiobook("The Kite Runner", "Khaled Hosseini", 2003, 12))
-
+    
     while True:
         print("Welcome To Tamara's Alcove")
         print("1. View All Books")
         print("2. Pick A Book To Read")
         print("3. Exit")
-
-        choice = input("Choose an option (1-3): ")
+        
+        choice = input("Choose an option (1-3):")
+        print("--------------------------------")
 
         if choice == "1":
             print("Library Catalogue:")
             for book in library.books:
                 print("-", book.get_details())
+                print("--------------------------------")
+
         elif choice == "2":
             title = input("Enter the title of the book you want to read: ")
             book = library.find_book(title)
@@ -33,12 +36,13 @@ def main():
                 print(book.read_book())
             else:
                 print("Book Not Found.") 
+
         elif choice == "3":
             print("Exiting Tamara's Alcove. See you next time. Bye!")
             break
         else:
             print("Invalid choice. Please try again.")
-
+            print("--------------------------------")
 if __name__ == "__main__":
     main()
                     
